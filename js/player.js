2d5459277fed6eea1216e36aa2b3037c5dbd8a93
ayly2341 <2502275258@qq.com>
2024-11-22 15:21:31 +0800

    Site updated: 2024-11-22 15:21:30

--- js/player.js
+++ js/player.js
@@ -372,3 +372,15 @@ new Player();
 document.addEventListener('pjax:complete', (e) => {
     new Player();
 })
+
+
+if (document.querySelector('#bber-talk')) {
+      var swiper = new Swiper('.swiper-container', {
+        direction: 'vertical', // 垂直切换选项
+        loop: true,
+        autoplay: {
+        delay: 3000,
+        pauseOnMouseEnter: true
+      },
+      });
+    }
