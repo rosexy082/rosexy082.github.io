8fde8d586b375fdf48015fa43802a4c259db81c4
ayly2341 <2502275258@qq.com>
2024-11-22 12:26:50 +0800

    Site updated: 2024-11-22 12:26:49

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,8 @@
+//51统计
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3KOJne8bzHjejaBV\",ck:\"3KOJne8bzHjejaBV\"});</script>")
+
+
+
 var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
 var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
     "menus_item_child"
